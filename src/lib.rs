65d3e48b0df7def3f351245fd5d6a4b3580519e9
Arseny Mitin <mitinarseny@gmail.com>
2025-06-17 15:22:05 +0400

    feat: UnstructuredExt

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,7 @@
 #![doc = include_str!("../README.md")]
 //! ## Example
 //! ```rust
-//! # use arbitrary::{Arbitrary, Error, Unstructured};
-//! # use arbitrary_with::{As, DisplayFromStr};
+//! # use arbitrary_with::{Arbitrary, As, DisplayFromStr, Error, Unstructured};
 //! #[derive(Arbitrary)]
 //! struct A {
 //!     #[arbitrary(with = As::<Option<DisplayFromStr<i32>>>::arbitrary)]
@@ -44,5 +43,69 @@ pub trait ArbitraryAs<'a, T> {
     #[inline]
     fn try_size_hint_as(depth: usize) -> Result<(usize, Option<usize>), MaxRecursionReached> {
         Ok(Self::size_hint_as(depth))
+    }
+}
+
+pub trait UnstructuredExt<'a> {
+    fn arbitrary_as<T, As>(&mut self) -> Result<T>
+    where
+        As: ArbitraryAs<'a, T>;
+
+    fn arbitrary_len_as<ElementType, ElementAs>(&mut self) -> Result<usize>
+    where
+        ElementAs: ArbitraryAs<'a, ElementType>;
+
+    fn arbitrary_iter_as<ElementType, ElementAs>(
+        &mut self,
+    ) -> Result<impl Iterator<Item = Result<ElementType>>>
+    where
+        ElementAs: ArbitraryAs<'a, ElementType>;
+
+    fn arbitrary_take_rest_iter_as<ElementType, ElementAs>(
+        self,
+    ) -> Result<impl Iterator<Item = Result<ElementType>>>
+    where
+        ElementAs: ArbitraryAs<'a, ElementType>;
+}
+
+impl<'a> UnstructuredExt<'a> for Unstructured<'a> {
+    #[inline]
+    fn arbitrary_as<T, As>(&mut self) -> Result<T>
+    where
+        As: ArbitraryAs<'a, T>,
+    {
+        self.arbitrary::<AsWrap<T, As>>().map(AsWrap::into_inner)
+    }
+
+    #[inline]
+    fn arbitrary_len_as<ElementType, ElementAs>(&mut self) -> Result<usize>
+    where
+        ElementAs: ArbitraryAs<'a, ElementType>,
+    {
+        self.arbitrary_len::<AsWrap<ElementType, ElementAs>>()
+    }
+
+    #[inline]
+    fn arbitrary_iter_as<ElementType, ElementAs>(
+        &mut self,
+    ) -> Result<impl Iterator<Item = Result<ElementType>>>
+    where
+        ElementAs: ArbitraryAs<'a, ElementType>,
+    {
+        Ok(self
+            .arbitrary_iter::<AsWrap<ElementType, ElementAs>>()?
+            .map(|r| r.map(AsWrap::into_inner)))
+    }
+
+    #[inline]
+    fn arbitrary_take_rest_iter_as<ElementType, ElementAs>(
+        self,
+    ) -> Result<impl Iterator<Item = Result<ElementType>>>
+    where
+        ElementAs: ArbitraryAs<'a, ElementType>,
+    {
+        Ok(self
+            .arbitrary_take_rest_iter::<AsWrap<ElementType, ElementAs>>()?
+            .map(|r| r.map(AsWrap::into_inner)))
     }
 }
